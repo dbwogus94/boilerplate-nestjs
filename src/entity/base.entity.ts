13fefcbb9207aa062c0b86aaffa5acf07a4643ac
jae hyun <1994dbwogus@gmail.com>
2024-02-09 02:49:47 +0900

    feat: Entity Mysql 지원 컬럼으로 변경 및 uid 컬럼 추가

--- src/entity/base.entity.ts
+++ src/entity/base.entity.ts
@@ -1,6 +1,7 @@
 import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
 import {
+  Column,
   CreateDateColumn,
   DeleteDateColumn,
   PrimaryGeneratedColumn,
@@ -8,23 +9,28 @@ import {
 } from 'typeorm';
 
 export class BaseEntity {
+  @ApiProperty({ description: '엔티티 uid', type: String })
+  @Expose()
+  @PrimaryGeneratedColumn('uuid')
+  readonly uid: string;
+
   @ApiProperty({ description: '엔티티 id', type: Number })
   @Expose()
-  @PrimaryGeneratedColumn()
-  id: number;
+  @Column('int', { generated: 'increment', unique: true })
+  readonly id: number;
 
   @ApiProperty({ description: '생성일', type: Date })
   @Expose()
-  @CreateDateColumn({ type: 'timestamptz' })
-  createdAt: Date;
+  @CreateDateColumn({ type: 'datetime' })
+  readonly createdAt: Date;
 
   @ApiProperty({ description: '수정일', type: Date })
   @Expose()
-  @UpdateDateColumn({ type: 'timestamptz' })
-  updatedAt: Date;
+  @UpdateDateColumn({ type: 'datetime' })
+  readonly updatedAt: Date;
 
   @ApiHideProperty()
   @Exclude()
-  @DeleteDateColumn({ type: 'timestamptz' })
+  @DeleteDateColumn({ type: 'datetime' })
   deletedAt: Date;
 }
