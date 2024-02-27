3bfe421fa36acb7354ba5e2a9520bd02171a6a6c
jae hyun <1994dbwogus@gmail.com>
2024-02-28 03:57:42 +0900

    feat: Entity와 dto에 정의된 데코레이터 수정

--- src/domain/user/dto/response/post-user-response.dto.ts
+++ src/domain/user/dto/response/post-user-response.dto.ts
@@ -1,18 +1,15 @@
-import { ApiProperty, PickType } from '@nestjs/swagger';
+import { PickType } from '@nestjs/swagger';
 
-import { defaultResponseProperties } from '@app/common';
+import { RestApiStringProperty, defaultResponseProperties } from '@app/common';
 import { UserEntity } from '@app/entity';
-import { Expose } from 'class-transformer';
 
 export class PostUserResponseDTO extends PickType(UserEntity, [
   ...defaultResponseProperties,
   'nickname',
 ]) {
-  @ApiProperty({
+  @RestApiStringProperty({
     description: 'jwt 토큰',
-    type: String,
     default: 'jwt',
   })
-  @Expose()
   token: string;
 }
